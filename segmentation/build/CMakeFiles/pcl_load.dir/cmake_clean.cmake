a265e076a1eb56c113787045485015b29faee396
sorapon <o104111s@mail.kyutech.jp>
2019-05-22 03:21:00 +0900

    [add] segmentation pcd data

---
+++ segmentation/build/CMakeFiles/pcl_load.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pcl_load.dir/pcl_load.cpp.o"
+  "pcl_load.pdb"
+  "pcl_load"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pcl_load.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
